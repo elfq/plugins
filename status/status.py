e545ede30476e15b40caa2a5fb47c7bea6e41b9d
elf <49889930+elfq@users.noreply.github.com>
2021-03-01 16:24:09 -0500

    Create status.py

---
+++ status/status.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class StatusPlugin(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command(name = "statusloop")
+  async def status_task(self, ctx, *, activity1, *, activity2, seconds1=15, seconds2=15):
+    while True:
+        await bot.change_presence(activity=discord.Game(name=activity1))
+        await asyncio.sleep(seconds1)
+        await bot.change_presence(activity=discord.Game(name=activity2))
+        await asyncio.sleep(seconds2)
+        
+  @commands.Cog.listener()
+  async def on_ready():
+    self.bot.loop.create_task(status_task())
+    
+    
+def setup(bot):
+  bot.add_cog(StatusPlugin(bot))
+        
+ 
+    
